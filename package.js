8fbee88abc0ea05d51b9b5876d134896d6e91494
Sacha Greif <sacha357@gmail.com>
2016-03-29 14:13:03 +0900

    use fourseven:scss 3.4.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'studiointeract:react-accounts-ui-basic',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Basic – Accounts UI for React in Meteor 1.3',
   git: 'https://github.com/studiointeract/react-accounts-ui-basic',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.3');
   api.use('ecmascript');
   api.use('underscore');
-  api.use('fourseven:scss@0.9.6');
+  api.use('fourseven:scss@3.4.1');
   api.use('studiointeract:react-accounts-ui@1.0.2');
 
   api.imply('session');
